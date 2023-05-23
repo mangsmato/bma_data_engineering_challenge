2ccc5191c00f660926ef86afbf210c0ec56747a2
Mang'oli Martin <mangsmato@gmail.com>
2023-05-23 20:11:33 +0300

    saved the data in the db

--- db_connection.py
+++ db_connection.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 try:
     # import the library to connect to db
     import psycopg2
-    import sqlalchemy
     
     # Establish a connection to the PostgreSQL server
     host = "localhost"
     user = "postgres"
-    password = "mm4@syIT"
+    password = "mm4easy1T"
     conn = psycopg2.connect(host=host, user=user, password=password)
 
     # Create a cursor object
@@ -21,29 +20,48 @@ try:
     # Specify the database name
     database_name = "pcard_expenditures"
 
-    # Check if the database already exists
-    cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s", (database_name,))
-    exists = cursor.fetchone()
-
-    # If the database doesn't exist, create it
-    if not exists:
-        cursor.execute("COMMIT")  # End any active transaction
-        cursor.execute("CREATE DATABASE {}".format(database_name))
-        print(f"{database_name} Database created successfully.")
-    else:
-        print(f"{database_name} Database already exists")
-
+    # If the database doesn't exist, create it    
+    cursor.execute("COMMIT")  # End any active transaction
+    cursor.execute("CREATE SCHEMA IF NOT EXISTS {}".format(database_name))
+    
+    # Specify the table name
+    database_name = "pcard_expenditures"
+    table_name = "raw_data_transactions"
+    # create the table that holds raw data
+    table_sql = f"""
+    CREATE TABLE IF NOT EXISTS {database_name}.{table_name} (
+        "Division" VARCHAR(150), 
+        "BatchTransactionID" VARCHAR(150), 
+        "TransactionDate" VARCHAR(150), 
+        "CardPostingDt" VARCHAR(150), 
+        "MerchantName" VARCHAR(150), 
+        "TransactionAmt" VARCHAR(150), 
+        "TrxCurrency" VARCHAR(150), 
+        "OriginalAmount" VARCHAR(150), 
+        "OriginalCurrency" VARCHAR(150), 
+        "GLAccount" VARCHAR(150), 
+        "GLAccountDescription" VARCHAR(150), 
+        "CostCenterWBSElementOrder" VARCHAR(150), 
+        "CostCenterWBSElementOrderDescription" VARCHAR(150), 
+        "MerchantType" VARCHAR(150), 
+        "MerchantTypeDescription" VARCHAR(150), 
+        "Purpose" VARCHAR(150), 
+        "SourceFileName" VARCHAR(150)
+    )
+    """
+    cursor.execute(table_sql)     
+   
     # Commit the changes
     conn.commit()
     
-    # create an sqlachemy object
-    # connection_uri = f"postgresql://{user}:{password}@{host}:5432/{database_name}"    
-    connection_uri = "postgresql://postgres:mm4easy!T@localhost:5432/pcard_expenditures"
-    db_conn = sqlalchemy.create_engine(connection_uri)
+    # save a new connection for later use
+    db_conn = psycopg2.connect(host=host, database=database_name, user=user, password=password)
+    
+    print('Database connected successfully')
 
     # Close the cursor and the connection
-#     cursor.close()
-#     conn.close()
+    cursor.close()
+    conn.close()
     
 except Exception as e:
         print(f"An error occurred {e}")
